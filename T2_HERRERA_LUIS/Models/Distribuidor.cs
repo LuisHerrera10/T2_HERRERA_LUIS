b508e88864766b26470d29dc7ae73e17c7918160
LuisHerrera10 <luisherreranumberfull@gmail.com>
2024-05-29 09:56:18 -0500

    COMMIT DE DEMOSTRACION

--- T2_HERRERA_LUIS/Models/Distribuidor.cs
+++ T2_HERRERA_LUIS/Models/Distribuidor.cs
@@ -4,8 +4,6 @@ namespace T2_HERRERA_LUIS.Models
 {
     public class Distribuidor
     {
-        
-        
         public int ID { get; set; }
 
         [Required(ErrorMessage ="El nombre de distribuidor es obligatorio")]
@@ -17,14 +15,12 @@ namespace T2_HERRERA_LUIS.Models
         [Required(ErrorMessage = "El telefono de distribuidor es obligatorio")]
         public int Telefono { get; set; }
 
-        [Range(1900, 3000,ErrorMessage ="El rango es de 1900 a 3000"),Required(ErrorMessage = "El nombre de distribuidor es obligatorio")]
+        [Range(1900, 3000,ErrorMessage ="El rango es de 1900 a 3000"),
+         Required(ErrorMessage = "El nombre de distribuidor es obligatorio")]
         public int AnioInicioOperacion { get; set; }
 
 
         [Required(ErrorMessage = "El nombre de contacto es obligatorio")]
         public string Contacto { get; set; }
-
-
-
     }
 }
--- T2_HERRERA_LUIS/obj/Debug/net8.0/T2_HERRERA_LUIS.AssemblyInfo.cs
+++ T2_HERRERA_LUIS/obj/Debug/net8.0/T2_HERRERA_LUIS.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("T2_HERRERA_LUIS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+808a17195ff6dad542716f91fbd417cb72ccfcf9")]
 [assembly: System.Reflection.AssemblyProductAttribute("T2_HERRERA_LUIS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("T2_HERRERA_LUIS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
